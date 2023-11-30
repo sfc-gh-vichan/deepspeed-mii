d232a91fe0ebadd8e189dbc291baec69e8766248
Vincent <vincent.chan@snowflake.com>
2023-11-30 14:53:33 -0800

    wip

--- relax/flask/main.py
+++ relax/flask/main.py
@@ -1,31 +1,16 @@
-from quart import Quart, request
+from flask import Flask, request
 from relax.flask.handler import Handler
 from relax.flask.args import args
-from quart import request
-from hypercorn.config import Config
-import asyncio
-from hypercorn.asyncio import serve
-
-from multiprocessing import Process, Lock
-
-mutex = Lock()
-
-
-config = Config()
-config.bind = [f"localhost:{args.port}"]
 
 handler = Handler()
 
-app = Quart(__name__)
+app = Flask(__name__)
 
-@app.post("/generate")
-async def _generate():
-    data = await request.get_json()
-    mutex.acquire()
+@app.route('/generate', methods=['POST'])
+def _generate():
+    data = request.get_json()
     model_responses = handler.client.generate(data["prompts"])
-    mutex.release()
     return {"model_outputs": [resp.generated_text for resp in model_responses]}
 
-
 if __name__ == "__main__":
-    asyncio.run(serve(app, config))
+    app.run(host="0.0.0.0", port=args.port)
