15c3c1042b51d87659f653f469540cc5a3d8af22
Vincent <vincent.chan@snowflake.com>
2023-11-30 14:56:30 -0800

    wip

--- relax/flask/main.py
+++ relax/flask/main.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
 from relax.flask.handler import Handler
 from relax.flask.args import args
+from threading import Thread, Lock
+
+mutex = Lock()
 
 handler = Handler()
 
@@ -9,7 +12,9 @@ app = Flask(__name__)
 @app.route('/generate', methods=['POST'])
 def _generate():
     data = request.get_json()
+    mutex.acquire()
     model_responses = handler.client.generate(data["prompts"])
+    mutex.release()
     return {"model_outputs": [resp.generated_text for resp in model_responses]}
 
 if __name__ == "__main__":
