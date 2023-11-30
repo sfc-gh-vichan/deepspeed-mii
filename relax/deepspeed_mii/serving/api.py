b87e110bd82ad5dc2be0c3c949205556b1d8f155
Vincent <vincent.chan@snowflake.com>
2023-11-29 18:14:38 -0800

    wip

--- relax/deepspeed_mii/serving/api.py
+++ relax/deepspeed_mii/serving/api.py
@@ -2,8 +2,6 @@ from typing import Any
 from fastapi import FastAPI
 from relax.deepspeed_mii.serving.handler import Handler
 from relax.deepspeed_mii.serving.schema import ModelInferRequest
-import nest_asyncio
-nest_asyncio.apply()
 
 handler = Handler()
 app = FastAPI()
