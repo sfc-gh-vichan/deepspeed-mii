6267e65dfc126cbf254e5514d64c44adb3c2b738
Vincent <vincent.chan@snowflake.com>
2023-12-05 00:19:19 -0800

    wip

--- relax/test_vllm/main.py
+++ relax/test_vllm/main.py
@@ -1,10 +1,10 @@
 import argparse
-from time import sleep
 import asyncio
 
 from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm.sampling_params import SamplingParams
+from vllm.model_executor.parallel_utils.parallel_state import destroy_model_parallel
 
 
 parser = argparse.ArgumentParser()
@@ -15,9 +15,10 @@ args = parser.parse_args()
 engine_args = AsyncEngineArgs.from_cli_args(args)
 engine = AsyncLLMEngine.from_engine_args(engine_args)
 
-# Requests
 
-sampling_params = SamplingParams(temperature=0,  # get rid of nondeterminism.
+# get rid of nondeterminism.
+sampling_params = SamplingParams(
+    temperature=0,  
     top_p=1.0,
     top_k=-1,
     max_tokens=1024
@@ -48,3 +49,13 @@ async def main():
         print(result)
 
 asyncio.run(main())
+
+try:
+    # Destroy
+    destroy_model_parallel()
+    del llm
+    gc.collect()
+    torch.cuda.empty_cache()
+    torch.distributed.destroy_process_group()
+except Exception as e:
+    print(f'failed to destroy vllm: {e}')
