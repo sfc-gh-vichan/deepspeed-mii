54153dfa0a1e53bad0e6a3d0b11d4273973262b8
Vincent <vincent.chan@snowflake.com>
2023-12-05 02:26:19 -0800

    wip

--- relax/test_vllm/main.py
+++ relax/test_vllm/main.py
@@ -32,27 +32,30 @@ results_generator = engine.generate("asdf", sampling_params, "")
 
 async def stream_results():
     full_output = ""
-    first = True
-    start = time.time()
-    ttft = None
-    total_time = None
     async for request_output in results_generator:
-        if first:
-            ttft = time.time() - start
-            first = False
         output = ""
         text_outputs = [output.text for output in request_output.outputs]
         for texts in text_outputs:
             output += texts[len(full_output):]
         full_output += output
         yield output
+
+
+async def main():
+    first = True
+    start = time.time()
+    ttft = None
+    total_time = None
+    full_output = result
+    for result in stream_results():
+        if first:
+            ttft = time.time() - start
+            first = False
+        full_output += result
     total_time = time.time() - start
     print("ttft: " + "{:.3f}".format(ttft))
     print("total_time: " + "{:.2f}".format(total_time))
 
-
-async def main():
-    stream_results()
 
 asyncio.run(main())
 
