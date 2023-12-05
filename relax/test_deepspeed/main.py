c4e14ae8a01c36dee66736b7e1a2b1978e389444
Vincent <vincent.chan@snowflake.com>
2023-12-05 10:38:26 -0800

    wip

--- relax/test_deepspeed/main.py
+++ relax/test_deepspeed/main.py
@@ -1,11 +1,10 @@
 from dataclasses import dataclass
-# import json
+import json
 import os
 from pathlib import Path
 from relax.test_deepspeed.args import args
-import argparse
 
-# from dacite import from_dict
+from dacite import from_dict
 import mii
 
 @dataclass
@@ -20,28 +19,25 @@ async def stream(response):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str)
-    args = parser.parse_args()
-    # path = args.model_repository
-    # p = Path(path)
-    # model_name = ""
-    # for f in p.iterdir():
-    #     if f.is_dir():
-    #         model_name = f.name
-    #         break
-    # if len(model_name) == 0:
-    #     # model does not exist
-    #     pass
-    # deployment_config_file_path = os.path.join(path, model_name, "deployment_config.json")
-    # f = open(deployment_config_file_path)
-    # config = json.load(f)
-    # deployment_config = from_dict(data_class=DeploymentConfig, data=config)
+    path = args.model_repository
+    p = Path(path)
+    model_name = ""
+    for f in p.iterdir():
+        if f.is_dir():
+            model_name = f.name
+            break
+    if len(model_name) == 0:
+        # model does not exist
+        pass
+    deployment_config_file_path = os.path.join(path, model_name, "deployment_config.json")
+    f = open(deployment_config_file_path)
+    config = json.load(f)
+    deployment_config = from_dict(data_class=DeploymentConfig, data=config)
     client = mii.serve(
-        model_name_or_path=os.path.join(args.model),
-        deployment_name=args.model_name,
-        tensor_parallel=1,
-        replica_num=1,
+        model_name_or_path=os.path.join(path, model_name),
+        deployment_name=deployment_config.model_name,
+        tensor_parallel=deployment_config.tensor_parallel,
+        replica_num=deployment_config.replica_num,
     )
     results = client.generate(
         prompts="asdf",
@@ -49,4 +45,4 @@ if __name__ == "__main__":
     )
     print(results)
     client.terminate_server()
-    # f.close()
+    f.close()
