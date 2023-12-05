6f8c2ad6d162652e97ed1f02954f8513377e2038
Vincent <vincent.chan@snowflake.com>
2023-12-05 13:29:03 -0800

    wip

--- relax/test_deepspeed/main.py
+++ relax/test_deepspeed/main.py
@@ -14,39 +14,47 @@ class DeploymentConfig:
     replica_num: int
 
 if __name__ == "__main__":
-    path = args.model_repository
-    p = Path(path)
-    model_name = ""
-    for f in p.iterdir():
-        if f.is_dir():
-            model_name = f.name
-            break
-    if len(model_name) == 0:
-        # model does not exist
+    client = None
+    f = None
+    try:
+        path = args.model_repository
+        p = Path(path)
+        model_name = ""
+        for f in p.iterdir():
+            if f.is_dir():
+                model_name = f.name
+                break
+        if len(model_name) == 0:
+            # model does not exist
+            pass
+        deployment_config_file_path = os.path.join(path, model_name, "deployment_config.json")
+        f = open(deployment_config_file_path)
+        config = json.load(f)
+        deployment_config = from_dict(data_class=DeploymentConfig, data=config)
+        client = mii.serve(
+            model_name_or_path=os.path.join(path, model_name),
+            deployment_name=deployment_config.model_name,
+            tensor_parallel=deployment_config.tensor_parallel,
+            replica_num=deployment_config.replica_num,
+        )
+
+        out_tokens = []
+        def callback(response):
+            print(type(response[0]))
+            print(f"recv: {response[0]}")
+            out_tokens.append(response[0])
+
+        result_queue = []
+        results = client.generate(
+            prompts="asdf",
+            streaming_fn=callback,
+        )
+
+        print(''.join(out_tokens))
+    except:
         pass
-    deployment_config_file_path = os.path.join(path, model_name, "deployment_config.json")
-    f = open(deployment_config_file_path)
-    config = json.load(f)
-    deployment_config = from_dict(data_class=DeploymentConfig, data=config)
-    client = mii.serve(
-        model_name_or_path=os.path.join(path, model_name),
-        deployment_name=deployment_config.model_name,
-        tensor_parallel=deployment_config.tensor_parallel,
-        replica_num=deployment_config.replica_num,
-    )
-
-    out_tokens = []
-    def callback(response):
-        print(f"recv: {response[0]}")
-        out_tokens.append(response[0])
-
-    result_queue = []
-    results = client.generate(
-        prompts="asdf",
-        streaming_fn=callback,
-    )
-
-    print(''.join(out_tokens))
-
-    client.terminate_server()
-    f.close()
+    finally:
+        if client is not None:
+            client.terminate_server()
+        if f is not None:
+            f.close()
