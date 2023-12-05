d51836694a76ea81b913df19649f9159f0ba75c5
Vincent <vincent.chan@snowflake.com>
2023-12-05 09:42:32 -0800

    wip

--- relax/test_vllm/main.py
+++ relax/test_vllm/main.py
@@ -14,7 +14,6 @@ from transformers import AutoTokenizer
 parser = argparse.ArgumentParser()
 parser.add_argument("--host", type=str, default="0.0.0.0")
 parser.add_argument("--port", type=int, default=8000)
-parser.add_argument("--model", type=str)
 parser = AsyncEngineArgs.add_cli_args(parser)
 args = parser.parse_args()
 engine_args = AsyncEngineArgs.from_cli_args(args)
