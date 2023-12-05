3707f6b20d3ec2478bce71bc32ca630e847169f0
Vincent <vincent.chan@snowflake.com>
2023-12-05 01:59:58 -0800

    wip

--- relax/test_vllm/main.py
+++ relax/test_vllm/main.py
@@ -2,6 +2,7 @@ import argparse
 import asyncio
 import gc
 import torch
+import time
 
 from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.engine.async_llm_engine import AsyncLLMEngine
@@ -31,20 +32,24 @@ results_generator = engine.generate("asdf", sampling_params, "")
 
 async def stream_results():
     full_output = ""
+    first = True
+    start = time.time()
+    ttft = None
+    total_time = None
     async for request_output in results_generator:
+        if first:
+            ttft = time.time() - start
+            first = False
         output = ""
         text_outputs = [output.text for output in request_output.outputs]
         for texts in text_outputs:
             output += texts[len(full_output):]
         full_output += output
         yield output
-    print(full_output)
+    total_time = time.time() - start
+    print("ttft: " + "{:.2f}".format(ttft))
+    print("total_time: " + "{:.2f}".format(total_time))
 
-# results = stream_results()
-
-# print(results)
-
-# sleep(1000)
 
 async def main():
     async for result in stream_results():
