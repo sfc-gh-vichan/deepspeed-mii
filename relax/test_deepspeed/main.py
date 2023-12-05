f9c209ff085ef590195516b60be5cea51952679d
Vincent <vincent.chan@snowflake.com>
2023-12-05 10:22:49 -0800

    wip

--- relax/test_deepspeed/main.py
+++ relax/test_deepspeed/main.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass
-import json
+# import json
 import os
 from pathlib import Path
 from relax.test_deepspeed.args import args
 
-from dacite import from_dict
+# from dacite import from_dict
 import mii
 
 @dataclass
@@ -19,31 +19,30 @@ async def stream(response):
 
 
 if __name__ == "__main__":
-    path = args.model_repository
-    p = Path(path)
-    model_name = ""
-    for f in p.iterdir():
-        if f.is_dir():
-            model_name = f.name
-            break
-    if len(model_name) == 0:
-        # model does not exist
-        pass
-    deployment_config_file_path = os.path.join(path, model_name, "deployment_config.json")
-    f = open(deployment_config_file_path)
-    config = json.load(f)
-    deployment_config = from_dict(data_class=DeploymentConfig, data=config)
+    # path = args.model_repository
+    # p = Path(path)
+    # model_name = ""
+    # for f in p.iterdir():
+    #     if f.is_dir():
+    #         model_name = f.name
+    #         break
+    # if len(model_name) == 0:
+    #     # model does not exist
+    #     pass
+    # deployment_config_file_path = os.path.join(path, model_name, "deployment_config.json")
+    # f = open(deployment_config_file_path)
+    # config = json.load(f)
+    # deployment_config = from_dict(data_class=DeploymentConfig, data=config)
     client = mii.serve(
-        model_name_or_path=os.path.join(path, model_name),
-        deployment_name=deployment_config.model_name,
-        tensor_parallel=deployment_config.tensor_parallel,
-        replica_num=deployment_config.replica_num,
-        enable_restful_api=True,
-        restful_api_port=args.port,
+        model_name_or_path=os.path.join(args.model),
+        deployment_name=args.model_name,
+        tensor_parallel=1,
+        replica_num=1,
     )
-    client.generate(
+    results = client.generate(
         prompts="asdf",
         streaming_fn=stream,
     )
+    print(results)
     client.terminate()
-    f.close()
+    # f.close()
