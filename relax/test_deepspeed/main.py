daae77156d192ef01cdfc3fe14d9d56c8714f76f
Vincent <vincent.chan@snowflake.com>
2023-12-08 03:19:02 -0800

    wip

--- relax/test_deepspeed/main.py
+++ relax/test_deepspeed/main.py
@@ -1,18 +1,33 @@
-from dataclasses import dataclass
-import json
-import os
-from pathlib import Path
-from relax.test_deepspeed.args import args
 import time
-
-from dacite import from_dict
+import argparse
 import mii
+from prompt_generator import PromptsGenerator
 
-@dataclass
-class DeploymentConfig:
-    model_name: str
-    tensor_parallel: int
-    replica_num: int
+MAX_SEQUENCE_LENGTH = 4096
+
+def list_of_ints(arg):
+    return list(map(int, arg.split(',')))
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Benchmark inference")
+    parser.add_argument("-k",
+                        "--max_new_tokens",
+                        type=int,
+                        default=1024)
+    parser.add_argument("-l",
+                        "--prompt_length",
+                        help="average number of tokens each prompt.",
+                        type=list_of_ints,
+                        default='1024')
+    parser.add_argument("-tp",
+                        "--tensor_parallel",
+                        type=int,
+                        help="Tensor parallelism",
+                        default='1')
+    parser.add_argument('--model', type=str, required=True, help="path to the model")
+
+    args, _ = parser.parse_known_args()
+    return args
 
 
 class CallbackObject:
@@ -25,25 +40,11 @@ if __name__ == "__main__":
     client = None
     f = None
     try:
-        path = args.model_repository
-        p = Path(path)
-        model_name = ""
-        for f in p.iterdir():
-            if f.is_dir():
-                model_name = f.name
-                break
-        if len(model_name) == 0:
-            # model does not exist
-            pass
-        deployment_config_file_path = os.path.join(path, model_name, "deployment_config.json")
-        f = open(deployment_config_file_path)
-        config = json.load(f)
-        deployment_config = from_dict(data_class=DeploymentConfig, data=config)
+        args = parse_args()
         client = mii.serve(
-            model_name_or_path=os.path.join(path, model_name),
-            deployment_name=deployment_config.model_name,
-            tensor_parallel=deployment_config.tensor_parallel,
-            replica_num=deployment_config.replica_num,
+            model_name_or_path=args.model,
+            deployment_name="llama",
+            tensor_parallel=args.tensor_parallel,
         )
 
         callback_object = CallbackObject()
@@ -59,14 +60,28 @@ if __name__ == "__main__":
             "top_p": 1.0,
         }
 
+        prompt_generator = PromptsGenerator(tokenizer_path=args.model)
+        prompts = []
+        for prompt_len in args.prompt_length:
+            prompts.append(prompt_generator.generate(
+                average_token=prompt_len,
+                variance=prompt_len*0.3,
+                max_token=MAX_SEQUENCE_LENGTH-args.max_new_tokens,
+                n=1,
+                show_progress=True,
+            ))
+
         start_time = time.time()
-        for _ in range(0, 10):
+        for i, prompt in enumerate(prompts):
+            print(f"SENDING NEXT PROMPT: {i}")
             client.generate(
-                prompts="Hello my name is",
+                prompts=prompt,
                 streaming_fn=callback,
                 **sampling_params,
             )
+        end_time = time.time()
         callback_object.ttft = callback_object.ttft - start_time
+        latency = end_time - start_time
 
         print([out_token.to_msg_dict() for out_token in callback_object.responses])
 
