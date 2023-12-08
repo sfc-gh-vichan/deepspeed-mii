db5e30d7e6a1bb42c002642e0533f3c5b4a502fa
Vincent <vincent.chan@snowflake.com>
2023-12-07 23:45:31 -0800

    wip

--- benchmark/benchmark_chat.py
+++ benchmark/benchmark_chat.py
@@ -315,6 +315,6 @@ if __name__ == "__main__":
 
     print('!!!---Printing results---!!!')
     # Output results as a csv
-    print('framework, num_prompts, avg_input, max_input, min_input, avg_output, max_output, min_output, latency(s), throughput, tensor_parallel')
+    print('framework, avg_input, max_input, min_input, avg_output, max_output, min_output, latency(s), throughput, tensor_parallel')
     for i in benchmarks:
         print(i)
