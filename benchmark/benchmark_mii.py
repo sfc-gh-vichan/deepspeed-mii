c7229a26894dd0aee12475bbd21bd022a67d8333
Vincent <vincent.chan@snowflake.com>
2023-12-09 13:42:38 -0800

    wip

--- benchmark/benchmark_mii.py
+++ benchmark/benchmark_mii.py
@@ -237,7 +237,7 @@ def run_mii_benchmarks(
                 average_token=prompt_length,
                 variance=prompt_length*0.3,
                 max_token=MAX_SEQUENCE_LENGTH-max_new_tokens,
-                n=20,
+                n=100,
                 show_progress=True,
             )
         )
