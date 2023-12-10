59ca927f0325e61e26db49116db11c4f1837999d
Vincent <vincent.chan@snowflake.com>
2023-12-09 16:56:40 -0800

    wip

--- benchmark/benchmark_vllm.py
+++ benchmark/benchmark_vllm.py
@@ -110,9 +110,8 @@ def run_stream(outputs, benchmark_queue: queue.Queue, query: Query):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     print("starting run")
-    loop.create_task(stream_results(outputs, benchmark_queue, query))
+    asyncio.ensure_future(loop.create_task(stream_results(outputs, benchmark_queue, query)))
     print("run ended")
-    loop.close()
 
 
 def run_vllm_benchmarks(
